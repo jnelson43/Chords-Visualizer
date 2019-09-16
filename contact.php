508d1c10571d28baa889480d79ea0a1028aef9d2
jnelson43 <jman.nelson43@gmail.com>
2019-09-16 11:44:41 -0500

    Replaced contact.php with updated version

--- contact.php
+++ contact.php
@@ -1,69 +1,38 @@
 <?php
-ini_set('display_errors','on');
-error_reporting(E_ALL);
-if(isset($_POST['email'])) {
-    // EDIT THE 2 LINES BELOW AS REQUIRED
-    $email_to = "jnelson3465@gmail.com";
-    $email_subject = "Chords Visualizer";
- 
-    function died($error) {
-        // your error code can go here
-        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
-        echo "These errors appear below.<br /><br />";
-        echo $error."<br /><br />";
-        echo "Please go back and fix these errors.<br /><br />";
-        die();
-    }
- 
- 
-    // validation expected data exists
-    if(!isset($_POST['email']) ||
-        !isset($_POST['message'])) {
-        died('You must enter your email and a message');       
-    }
- 
-     
-    $email_from = 'contact@chordsvisualizer.com'; // required
-    $message = $_POST['message']; // required
- 
-    $error_message = "";
-    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
- 
-  if(!preg_match($email_exp,$email_from)) {
-    $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
-  }
- 
-    $string_exp = "/^[A-Za-z .'-]+$/";
- 
-  if(strlen($message) < 2) {
-    $error_message .= 'The message you entered do not appear to be valid.<br />';
-  }
- 
-  if(strlen($error_message) > 0) {
-    died($error_message);
-  }
- 
-    $email_message = "Form details below.\n\n";
- 
-     
-    function clean_string($string) {
-      $bad = array("content-type","bcc:","to:","cc:","href");
-      return str_replace($bad,"",$string);
-    }
- 
-    $email_message .= "Email: ".clean_string($email_from)."\n";
-    $email_message .= "message: ".clean_string($message)."\n";
- 
-// create email headers
-$headers = 'From: '.$email_from."\r\n".
-'Reply-To: '.$email_from."\r\n" .
-'X-Mailer: PHP/' . phpversion();
-@mail($email_to, $email_subject, $email_message, $headers);  
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'vendor/autoload.php';
+$body = $_POST['message'];
+$from = $_POST['email'];
+
+$mail = new PHPMailer(); // create a new object
+try {
+    //Server settings
+    $mail->SMTPDebug = 0;                                       // Enable verbose debug output
+    $mail->isSMTP();                                            // Set mailer to use SMTP
+    $mail->Host       = 'email-smtp.us-west-2.amazonaws.com;email-smtp.us-west-2.amazonaws.com';  // Specify main and backup SMTP servers
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = 'AKIATG3SFWZO7NQA2MNG';                     // SMTP username
+    $mail->Password   = 'BO6aVKJu2wfWGEjoSDoUySmJLbf8UdExJLkMvHdTdo21';                               // SMTP password
+    $mail->SMTPSecure = 'tls';                                  // Enable TLS encryption, `ssl` also accepted
+    $mail->Port       = 587;                                    // TCP port to connect to
+
+    //Recipients
+    $mail->setFrom('info@chordsvisualizer.com');
+    $mail->addAddress('jnelson3465@gmail.com');     // Add a recipient
+    $mail->addReplyTo($from);
+
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Chords Visualizer';
+    $mail->Body    =  $body;
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
 ?>
- 
-<!-- include your own success html here -->
- 
-<html>
+    <html>
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -72,9 +41,6 @@ $headers = 'From: '.$email_from."\r\n".
   <link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script type='text/javascript' src="resumeSite.js"></script>
-  <!--<link rel="stylesheet" type="text/css" href="resumeSite.css">-->
-  <link rel="stylesheet" type="text/css" href="resumeSite.css">
 </head>
 <body>
 <article>
@@ -118,8 +84,65 @@ h2, p{
 }
 
 </style>
- 
-<?php
- 
+
+<?php     
+} catch (Exception $e) {
+?>
+
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Uh Oh!</title>
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+</head>
+<body>
+<article>
+  
+  <section>
+    <h2>Something went wrong</h2>
+    <p>This message was not sent correctly, feel free to email me directly at jnelson3465@gmail.com</p>
+    <form action='index.html'>
+     <button class="btnSuccess">Home</button>
+   </form>
+</body>
+</html>
+<style>
+.btnSuccess{
+  outline: none;
+  border: none;
+  cursor: pointer;
+  display: block;
+  position: relative;
+  background-color: #EC973A;
+  font-size: 16px;
+  font-weight: 300px;
+  color: white;
+  text-transform: uppercase;
+  letter-spacing: 2px;
+  padding: 25px 80px;
+  margin: 0 auto;
+  border-radius: 20px;
+  box-shadow: 0 6px#d28632;
+}
+.btnSuccess:hover {
+  box-shadow: 0 4px #d28632;
+  top: 2px;
+}
+.btnSuccess:active{
+  box-shadow: none;
+  top: 6px;
+}
+h2, p{
+	text-align: center;
+}
+
+</style>
+<?php     
 }
 ?>
+ 
+<!-- include your own success html here -->
